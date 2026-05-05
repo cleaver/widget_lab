14467ae2eeb5ad57ef9031788bd77de756026695
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2026-05-05 12:45:26 -0400

    feat: add app route logging

--- lib/config/router.dart
+++ lib/config/router.dart
@@ -1,3 +1,5 @@
+import 'dart:developer' as dev;
+
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widget_lab/screens/home/home_screen.dart';
@@ -5,21 +7,52 @@ import 'package:widget_lab/screens/topic_detail/topic_detail_screen.dart';
 import 'package:widget_lab/screens/topics/day01.dart';
 import 'package:widget_lab/services/topic_repository.dart';
 
+class RouteLogObserver extends NavigatorObserver {
+  void _log(String message) {
+    debugPrint('[router] $message');
+    dev.log(message, name: 'router');
+  }
+
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    _log(
+      'push: ${route.settings.name ?? route.settings.arguments ?? route.settings}',
+    );
+  }
+
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    _log(
+      'pop: ${route.settings.name ?? route.settings.arguments ?? route.settings}',
+    );
+  }
+
+  @override
+  void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
+    _log('replace: ${oldRoute?.settings} -> ${newRoute?.settings}');
+  }
+}
+
 GoRouter buildRouter(TopicRepository repository) {
   return GoRouter(
     initialLocation: '/',
+    debugLogDiagnostics: true,
+    observers: <NavigatorObserver>[RouteLogObserver()],
     routes: <RouteBase>[
       GoRoute(
+        name: 'home',
         path: '/',
         builder: (BuildContext context, GoRouterState state) {
           return HomeScreen(repository: repository);
         },
       ),
       GoRoute(
+        name: 'day1',
         path: '/topic/day-1',
         builder: (context, state) => Day01ProfileHeaderScreen(),
       ),
       GoRoute(
+        name: 'topicDetail',
         path: '/topic/:topicId',
         builder: (BuildContext context, GoRouterState state) {
           final String topicId = state.pathParameters['topicId'] ?? '';
